000f71519fdf58cec9d7efc17f90aed7b018023b
Kim, Shawn <skim245@go.pasadena.edu>
2024-09-13 00:46:45 -0700

    t

---
+++ src/main/java/shawnkim929/skslime/SkSlime.java
@@ -0,0 +1,18 @@
+package shawnkim929.skslime;
+
+import org.bukkit.event.Listener;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class SkSlime extends JavaPlugin implements Listener {
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
